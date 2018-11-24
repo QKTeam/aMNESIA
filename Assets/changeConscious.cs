2b7c4abcdff4b814b5e89f9e7852bfa8e7bb1828
RSzyh <750332908@qq.com>
2018-11-24 21:12:59 +0800

    add switch conscious(mode)

---
+++ Assets/changeConscious.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeConscious : MonoBehaviour {
+
+	public GameObject surfaceConscious;
+	public GameObject subConscious;
+	private bool isSubDisplay = false;
+
+	// Use this for initialization
+	void Start () {
+		surfaceConscious.SetActive(true);
+		subConscious.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("f")) {
+			isSubDisplay = !isSubDisplay;
+			ChangeMode();
+		}
+	}
+
+	void ChangeMode () {
+		if (isSubDisplay) {
+			subConscious.SetActive(true);
+			surfaceConscious.SetActive(false);
+		} else {
+			surfaceConscious.SetActive(true);
+			subConscious.SetActive(false);
+		}
+	}
+}
