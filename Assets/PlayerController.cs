11679921aab242233ca34a2984ae8212bbe334af
gongrongyun <1716175849@qq.com>
2018-11-24 20:10:49 +0800

    add a playerController script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    private bool inAir;
+    private float horizontal;
+    private Rigidbody2D rig;
+
+    public 
+
+	// Use this for initialization
+	void Start () {
+        rig = GetComponent<Rigidbody2D>();
+        jumpForce = 400f;
+        moveSpeed = 4.0f;
+        inAir = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && !inAir)
+        {
+            rig.AddForce(new Vector2(0, jumpForce));
+            inAir = true;
+        }
+        horizontal = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(horizontal * moveSpeed, rig.velocity.y);
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        inAir = false;
+    }
+}
