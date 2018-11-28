5ff9f138f65ed204058c8ef1e6903b1216fab926
RSzyh <750332908@qq.com>
2018-11-28 20:03:37 +0800

    add memory piece prefab and related scripts

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+	[SerializeField] private int memoryPieceNum = 3;
+
+	public void playerGetPiece()
+	{
+		--memoryPieceNum;
+	}
+}
