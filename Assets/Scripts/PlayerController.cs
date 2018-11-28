94fc57558cff18ab6657291329a2501da5d8f32f
RSzyh <750332908@qq.com>
2018-11-28 14:03:49 +0800

    finish player move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] private float moveSpeed = 2.5f;
-	[SerializeField] private float jumpForce = 300f;
+	[SerializeField] private float moveSpeed = 130f;
+	[SerializeField] private float jumpForce = 270f;
 	[Range(0, .3f)] [SerializeField] private float moveSmoothing = .05f;
 	[SerializeField] private LayerMask groundLayer;
 	[SerializeField] private Transform groundCheck;
@@ -13,8 +13,8 @@ public class PlayerController : MonoBehaviour
 	const float checkRadius = .01f;
 	private bool isGrounded;
 	private bool isAlive = true;
-	private float moveInput;
-	private float jumpInput;
+	private bool jump = false;
+	private float horizonMove = 0f;
 	private Rigidbody2D rb2d;
 	private Vector3 n_velocity = Vector3.zero;
 
@@ -25,12 +25,16 @@ public class PlayerController : MonoBehaviour
 
 	private void Update()
 	{
-		moveInput = Input.GetAxisRaw("Horizontal");
-		jumpInput = Input.GetAxisRaw("Vertical");
+		horizonMove = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.fixedDeltaTime;
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
 	}
 
 	private void FixedUpdate()
 	{
+		isGrounded = false;
 		// Judge if ground
 		Collider2D[] colliders =
 			Physics2D.OverlapCircleAll(groundCheck.position, checkRadius, groundLayer);
@@ -45,16 +49,17 @@ public class PlayerController : MonoBehaviour
 		if (isAlive)
 		{
 			Move();
+			jump = false;
 		}
 	}
 
 	private void Move()
 	{
-		Vector3 targetVelocity = new Vector2(moveInput * moveSpeed, rb2d.velocity.y);
+		Vector3 targetVelocity = new Vector2(horizonMove, rb2d.velocity.y);
 		rb2d.velocity =
 			Vector3.SmoothDamp(rb2d.velocity, targetVelocity, ref n_velocity, moveSmoothing);
 		// Handle jump
-		if (isGrounded && jumpInput > 0)
+		if (isGrounded && jump)
 		{
 			isGrounded = false;
 			rb2d.AddForce(Vector2.up * jumpForce);
