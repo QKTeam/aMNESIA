81b23908ba1ad0495b54f9c5af813654e85d523c
RSzyh <750332908@qq.com>
2018-11-29 19:58:47 +0800

    add float controller and floatboard prefab

---
+++ Assets/Scripts/FloatContoller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatContoller : MonoBehaviour
+{
+	[SerializeField] private string direction = "vertical";
+	[SerializeField] private float isToMax = 1f;
+	[SerializeField] private float floatSpeed = .8f;
+	[SerializeField] private float maxPos;
+	[SerializeField] private float minPos;
+
+	private void FixedUpdate()
+	{
+		if (direction == "vertical")
+		{
+			float diffY = floatSpeed * isToMax * Time.fixedDeltaTime;
+			if (transform.position.y + diffY > maxPos)
+			{
+				diffY = maxPos - transform.position.y;
+				isToMax = -1f;
+			}
+			else if (transform.position.y + diffY < minPos)
+			{
+				diffY = minPos - transform.position.y;
+				isToMax = 1f;
+			}
+			transform.position += Vector3.up * diffY;
+		}
+		else if (direction == "horizontal")
+		{
+			float diffX = floatSpeed * isToMax * Time.fixedDeltaTime;
+			if (transform.position.x + diffX > maxPos)
+			{
+				diffX = maxPos - transform.position.x;
+				isToMax = -1f;
+			}
+			else if (transform.position.x + diffX < minPos)
+			{
+				diffX = minPos - transform.position.x;
+				isToMax = 1f;
+			}
+			transform.position += Vector3.right * diffX;
+		}
+	}
+}
