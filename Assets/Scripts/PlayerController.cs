373ab3894b20b72c9b47674d15a8069b503c93e5
RSzyh <750332908@qq.com>
2018-12-01 12:17:20 +0800

    finish basic player floating

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,14 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private float moveSpeed = 100f;
 	[SerializeField] private float jumpForce = 250f;
 	[SerializeField] private float floatSpeed = .02f;
-	[SerializeField] private float floatHeight = .2f;
+	[SerializeField] private float floatHeight = .25f;
 	[Range(0, .3f)] [SerializeField] private float moveSmoothing = .05f;
 	[SerializeField] private LayerMask groundLayer;
 	[SerializeField] private Transform groundCheck;
+	[SerializeField] private Transform floatCheck;
 	[SerializeField] private MainController main;
 
-	const float checkRadius = .1f;
+	const float checkRadius = .01f;
 	private bool isGrounded;
 	private bool isFloating;
 	private bool jump = false;
@@ -21,6 +22,7 @@ public class PlayerController : MonoBehaviour
 	private float gravity;
 	private float floatPos;
 	private Rigidbody2D rb2d;
+	private Vector3 groundPos;// Player's position when grounding
 	private Vector3 deadPos;// Final position while player dead
 	private Vector3 n_velocity = Vector3.zero;
 
@@ -28,6 +30,8 @@ public class PlayerController : MonoBehaviour
 	{
 		rb2d = GetComponent<Rigidbody2D>();
 		gravity = rb2d.gravityScale;
+		floatCheck.position = groundCheck.position;
+		groundPos = transform.position;
 	}
 
 	private void Update()
@@ -46,8 +50,17 @@ public class PlayerController : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		isGrounded = false;
+		// Update floatCheck position
+		floatCheck.position = new Vector3(
+			groundCheck.position.x,
+			floatCheck.position.y,
+			floatCheck.position.z
+		);
+		// Update ground position
+		groundPos = new Vector3(transform.position.x, groundPos.y, groundPos.z);
+
 		// Judge if ground
+		isGrounded = false;
 		Collider2D[] colliders =
 			Physics2D.OverlapCircleAll(groundCheck.position, checkRadius, groundLayer);
 		for (int i = 0; i < colliders.Length; i++)
@@ -55,7 +68,23 @@ public class PlayerController : MonoBehaviour
 			if (colliders[i].gameObject != gameObject)
 			{
 				isGrounded = true;
+				floatCheck.position = groundCheck.position;
+				groundPos = transform.position;
 				floatPos = groundCheck.position.y + floatHeight;
+			}
+		}
+		// Judge if float over ground
+		if (isFloating)
+		{
+			StopFloating();
+			colliders =
+				Physics2D.OverlapCircleAll(floatCheck.position, checkRadius, groundLayer);
+			for (int i = 0; i < colliders.Length; i++)
+			{
+				if (colliders[i].gameObject != gameObject)
+				{
+					KeepFloating();
+				}
 			}
 		}
 		// Handle move
@@ -74,30 +103,23 @@ public class PlayerController : MonoBehaviour
 		if (horizonMove != 0)
 		{
 			// Make or keep player floating
-			if (isGrounded || isFloating)
+			if ((isGrounded || isFloating) && !jump)
 			{
-				rb2d.gravityScale = 0f;
-				isGrounded = false;
-				isFloating = true;
-				if (transform.position.y < floatPos)
-				{
-					transform.position += Vector3.up * floatSpeed;
-				}
-				if (transform.position.y > floatPos)
-				{
-					transform.position =
-						new Vector3(transform.position.x, floatPos, transform.position.z);
-				}
+				KeepFloating();
 			}
 		}
 		else
 		{
-			isFloating = false;
-			rb2d.gravityScale = gravity;
+			StopFloating();
 		}
 		// Handle jump
 		if (jump)
 		{
+			if (isFloating)
+			{
+				StopFloating();
+				transform.position = groundPos;
+			}
 			if (isGrounded)
 			{
 				isGrounded = false;
@@ -105,6 +127,28 @@ public class PlayerController : MonoBehaviour
 				jump = false;
 			}
 		}
+	}
+
+	private void KeepFloating()
+	{
+		rb2d.gravityScale = 0f;
+		isGrounded = false;
+		isFloating = true;
+		if (transform.position.y < floatPos)
+		{
+			transform.position += Vector3.up * floatSpeed;
+		}
+		if (transform.position.y > floatPos)
+		{
+			transform.position =
+				new Vector3(transform.position.x, floatPos, transform.position.z);
+		}
+	}
+
+	private void StopFloating()
+	{
+		isFloating = false;
+		rb2d.gravityScale = gravity;
 	}
 
 	private void OnTriggerEnter2D(Collider2D collider)
