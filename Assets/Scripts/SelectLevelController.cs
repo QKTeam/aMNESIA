ad0dae04efd0d64da75c4fa1d8d6e9f4ef43505d
RSzyh <750332908@qq.com>
2018-12-07 10:59:21 +0800

    disable select button when level is not match

--- Assets/Scripts/SelectLevelController.cs
+++ Assets/Scripts/SelectLevelController.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SelectLevelController : MonoBehaviour 
 {
-	public void LoadLevel(int num) 
+	[SerializeField] int levelNum;
+
+	private Button m_button;
+
+	private void Awake()
 	{
-		SceneManager.LoadScene("Level" + num);
+		m_button = GetComponent<Button> ();
+	}
+	private void Start()
+	{
+		if (levelNum > GlobalController.currentLevel)
+		{
+			m_button.interactable = false;
+		}
+	}
+
+	public void LoadLevel() 
+	{
+		SceneManager.LoadScene("Level" + levelNum);
 	}
 }
