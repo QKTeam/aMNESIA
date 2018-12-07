bd912cb551d72b2223749839be758ff211c9340f
unknown <925448163@qq.com>
2018-12-07 10:54:18 +0800

    add New game and Continue button and activejudger

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 public class MenuController : MonoBehaviour 
 {
+	public GameObject MenuButton;
+
 	private void Start()
 	{
 		GlobalController.LoadFile();
+		if (GlobalController.currentLevel == 0) 
+		{
+			MenuButton.SetActive(false);
+		}
 	}
 
 	public void NewGame()
