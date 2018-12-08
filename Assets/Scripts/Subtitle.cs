7a12fcc1ce0472612e34b7a3841c451b1ecd8e72
RSzyh <750332908@qq.com>
2018-12-08 13:51:46 +0800

    add subtitle class and finish start subtitle

---
+++ Assets/Scripts/Subtitle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Subtitle
+{
+	public enum SubtitleType
+	{
+		Start,// The text when scene starting
+		BindObject,// The text when colliding object
+	}
+	public string content;
+	public bool isForever;
+	public float delay;
+	public int nextIndex = -1;
+	public SubtitleType type;
+	public GameObject bindObject;
+}
--- Assets/Scripts/SubtitleController.cs
+++ Assets/Scripts/SubtitleController.cs
@@ -6,30 +6,60 @@ using UnityEngine.UI;
 public class SubtitleController : MonoBehaviour
 {
 	[SerializeField] private Text[] subtitle;
+	[SerializeField] private Subtitle[] startText;
+	[SerializeField] private Subtitle[] bindText;
 
-	private int writtenSerialNum = 0;
-	private int shownSerialNum = 1;
+	private int writtenIndex = 0;
+	private int shownIndex = 1;
 	private float m_delay = -1f;
+	private Subtitle currentText;
+
+	private void Start()
+	{
+		if (startText.Length > 0)
+		{
+			currentText = startText[0];
+			Showtext(currentText.content, currentText.delay);
+		}
+	}
 
 	public void Showtext(string text, float delaySecond)
 	{
-		subtitle[writtenSerialNum].text = text;
+		subtitle[writtenIndex].text = text;
 		if (delaySecond > 0)
 		{
 			m_delay = delaySecond / Time.fixedDeltaTime;
 		}
 		// Hide and show subtitle
-		subtitle[shownSerialNum].gameObject.SetActive(false);
-		subtitle[writtenSerialNum].gameObject.SetActive(true);
+		subtitle[shownIndex].gameObject.SetActive(false);
+		subtitle[writtenIndex].gameObject.SetActive(true);
 		// Swap serial number
-		int temp = shownSerialNum;
-		shownSerialNum = writtenSerialNum;
-		writtenSerialNum = temp;
+		int temp = shownIndex;
+		shownIndex = writtenIndex;
+		writtenIndex = temp;
 	}
 
 	public void Cleartext()
 	{
-		subtitle[shownSerialNum].gameObject.SetActive(false);
+		if (currentText.nextIndex >= 0)
+		{
+			switch (currentText.type.ToString())
+			{
+				case "Start":
+					currentText = startText[currentText.nextIndex];
+					break;
+				case "BindObject":
+					currentText = startText[currentText.nextIndex];
+					break;
+				default:
+					break;
+			}
+			Showtext(currentText.content, currentText.delay);
+		}
+		else
+		{
+			subtitle[shownIndex].gameObject.SetActive(false);
+		}
 	}
 
 	private void FixedUpdate()
@@ -40,7 +70,7 @@ public class SubtitleController : MonoBehaviour
 			--m_delay;
 		}
 		// Hide subtitle while delay equal 0
-		if (m_delay == 0)
+		if (m_delay == 0 && !currentText.isForever)
 		{
 			Cleartext();
 		}
