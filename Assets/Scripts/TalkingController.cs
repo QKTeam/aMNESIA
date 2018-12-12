7727dcc055778291426054114ab0e023cd8fa309
RSzyh <750332908@qq.com>
2018-12-13 07:15:13 +0800

    add Talking Scene

---
+++ Assets/Scripts/TalkingController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TalkingController : MonoBehaviour {
+
+	[SerializeField] private DoorController doorController;
+	private int count = 0;
+	private void FixedUpdate()
+	{
+		count++;
+		if (count == 600) {
+			doorController.OpenDoor();
+		}
+	}
+}
