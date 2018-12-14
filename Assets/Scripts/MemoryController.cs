5671862045f7b971591b5da752e669d41532d083
gongrongyun <1716175849@qq.com>
2018-12-14 16:44:44 +0800

    add memory particle and fix floors bug in Level5 and Level6

--- Assets/Scripts/MemoryController.cs
+++ Assets/Scripts/MemoryController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class MemoryController : MonoBehaviour
 {
 	[SerializeField] private MainController main;
+    [SerializeField] private ParticleSystem particle;
+
 	private void OnTriggerEnter2D(Collider2D collider)
 	{
 		if (collider.tag == "Player")
 		{
 			main.playerGetPiece();
+            Instantiate(particle, transform.position, Quaternion.identity);
 			Destroy(gameObject);
 		}
 	}
