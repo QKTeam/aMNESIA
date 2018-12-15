8b16f894b554f6ff86d9eebbdac41a940914e634
RSzyh <750332908@qq.com>
2018-12-15 14:44:58 +0800

    make memory and broken floor destroy themselves

---
+++ Assets/Scripts/TempParticleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempParticleController : MonoBehaviour
+{
+	[SerializeField] private float delay = 2f;
+
+	private void Awake()
+	{
+		delay = delay / Time.fixedDeltaTime;
+	}
+
+	private void FixedUpdate()
+	{
+		--delay;
+		if (delay <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
